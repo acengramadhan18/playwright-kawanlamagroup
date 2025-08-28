5bd18867f50ceaf894d58359acba597aa19486bc
FikriRamadhan <acengramadhan12@gmail.com>
2025-08-29 01:24:50 +0700

    Update test script

--- tests/scenario/login.spec.ts
+++ tests/scenario/login.spec.ts
@@ -7,12 +7,22 @@ test.describe("Login - Negative", () => {
     }) => {
         await loginPage.openWeb()
         await loginPage.logintanpaemail()
+        // Evidence screenshot
+        await loginPage.page.screenshot({
+        path: `evidence/negative-no-email.png`,
+        fullPage: true,
+    });
     });
     test("As user i can't login Kawan Lama Group Karir without input password", async ({
         loginPage
     }) => {
         await loginPage.openWeb()
         await loginPage.logintanpapassword()
+        // Evidence screenshot
+        await loginPage.page.screenshot({
+        path: `evidence/negative-no-password.png`,
+        fullPage: true,
+    });
     });
 });
 
@@ -22,6 +32,16 @@ test.describe("Login - Positive", () => {
     }) => {
         await loginPage.openWeb()
         await loginPage.login()
+        // Evidence screenshot setelah login
+        await loginPage.page.screenshot({
+        path: `evidence/positive-login-success.png`,
+        fullPage: true,
+    });
         await logout.logout()
+        // Evidence screenshot setelah logout
+        await loginPage.page.screenshot({
+        path: `evidence/positive-logout-success.png`,
+        fullPage: true,
+    });
     });
 });
